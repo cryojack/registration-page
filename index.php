47c34433dcea8169efda40c56c17c52049857791
cryojack <wayneskatesfast@gmail.com>
2020-05-13 08:59:31 +0530

    new overhaul

--- index.php
+++ index.php
@@ -27,4 +27,84 @@ $number = 12345 * 67890;
 echo substr($number, 3, 4);
 echo "<br>";
 echo "This is line " . __LINE__ . " of file " . __FILE__;
+echo "<br>";
+function longdate($timestamp)
+{
+return date("l jS", $timestamp);
+}
+echo longdate(time());
+echo "<br>";
+$a = "1000";
+$b = "+1000";
+if ($a == $b) echo "1";
+if ($a === $b) echo "2";
+echo "<br>";
+$thisnum = 0;
+while($thisnum < 10)
+{
+	$thisnum += 1;
+	echo "This nums is now " . $thisnum;
+	echo "<br>";
+}
+echo "<br>";
+$counter = 1;
+while($counter <= 10)
+{
+	echo $counter . " raised to 2 = " . $counter * $counter ."<br>";
+	$counter++;
+}
+
+echo "<br>";
+$a = 11;
+$b = 27;
+echo $a/$b . " -- " . (int)($a/$b);
+echo "<br>";
+echo "<br>";
+
+$name_array = array("MriDul","mUraLeEdhaRAN","SandY","DesIRE","tACO","fRIdaY");
+echo fix_name($name_array);
+function fix_name(&$arr)
+{
+	$counter1 = 0;
+	$arrcount = count($arr);
+	while($counter1 < $arrcount){
+		echo ucfirst(strtolower($arr[$counter1]));
+		echo "<br>";
+		$counter1 += 1;
+	}
+}
+
+echo "<br>";
+echo "<br>";
+
+if (function_exists("fix_name"))
+{
+	echo "It's there";
+}
+echo "<br>";
+echo phpversion();
+echo "<br>";
+
+class Person
+{
+	public $name, $age, $gender;
+
+	function display_person(){
+		echo "Name = ".$name;
+		echo "<br>";
+		echo "Age = ".$age;
+		echo "<br>";
+		echo "Gender = ".$gender;
+		echo "<br>";
+	}
+}
+$person = new Person;
+$person->name = "Mridul";
+$person->age = 28;
+$person->gender = "Male";
+echo "<br>";
+print_r($person);
+echo "<br>";
+
+
 ?>
